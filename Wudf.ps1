7204edbea555b08c0194d8fa980156771821bb25
Lopsy84 <122386309+Lopsy84@users.noreply.github.com>
2026-05-03 04:39:49 +0200

    Update Wudf.ps1

--- Wudf.ps1
+++ Wudf.ps1
@@ -1,52 +1,15 @@
-if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(
-    [Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    Write-Warning "Please run PowerShell as an Administrator."
-    Break
-}
-
-Get-PnpDevice | Where-Object { $_.Status -eq 'Error' -or $_.ConfigManagerErrorCode -eq 22 } | Select-Object FriendlyName, InstanceId, Status
-
-Write-Host "Scanning WUDFHost processes..." -ForegroundColor Cyan
-
-$wudfProcesses = Get-Process -Name "WUDFHost" -ErrorAction SilentlyContinue
-
-if (-not $wudfProcesses) {
-    Write-Host "No WUDFHost processes are currently running." -ForegroundColor Yellow
-    exit
-}
-
-$results = @()
-
-foreach ($proc in $wudfProcesses) {
-    try {
-        $driverModules = $proc.Modules | Where-Object {
-            $_.FileName -match "DriverStore\\FileRepository"
-        }
-
-        foreach ($mod in $driverModules) {
-            if ($mod.FileName -match "FileRepository\\([^\\]+)") {
-                $infFolder = $matches[1]
-
-                # Try to extract actual INF file name
-                $infName = ($infFolder -split ".inf_")[0]
-
-                $results += [PSCustomObject]@{
-                    ProcessId = $proc.Id
-                    InfFile   = $infName
-                }
-
-            }
-        }
-    } catch {
-        Write-Warning "Access denied inspecting PID $($proc.Id)"
+handle -a -p wudfhost | Select-String "Enum" | ForEach-Object {
+    $Deviceid = $_.Line -replace '.*\\Enum\\', '' -replace '\\Device Parameters.*', ''
+    $name = (Get-PnpDevice -DeviceId $Deviceid).Friendlyname
+    $response = Read-Host "Are you sure you want to disable device '$name'? (Y/N)"
+        if ($response -match '^[yY]') {
+        Write-Host "Disabling $Deviceid..." -ForegroundColor Yellow
+        pnputil.exe /disable-device $Deviceid /force
+        Write-Host "Done." -ForegroundColor Green
+    } else {
+        Write-Host "Skipping $Deviceid..." -ForegroundColor Cyan
     }
-    $Deviceid = Get-PnpDevice | Where-Object { $_.Service -like $infName } | Select-Object InstanceId
-    $Deviceid= ($Deviceid -split "=")[1]
-    $Deviceid= ($Deviceid -split "}")[0]
-    pnputil.exe /disable-device $Deviceid /force
-
 }
-if ($results.Count -eq 0) {
-    Write-Host "No INF files found."
-    exit
-}
+Write-Host "Disabled Drivers:"
+Get-PnpDevice | Where-Object { $_.ConfigManagerErrorCode -eq 22 }
+Get-PnpDevice | Where-Object { $_.ConfigManagerErrorCode -eq 22 } | Enable-PnpDevice
