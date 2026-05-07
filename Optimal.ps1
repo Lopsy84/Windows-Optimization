3dd93b9df700e69ea2282842f3921cd600f143bf
Lopsy84 <122386309+Lopsy84@users.noreply.github.com>
2026-05-07 16:07:25 +0200

    Update Optimal.ps1

--- Optimal.ps1
+++ Optimal.ps1
@@ -1,19 +1,16 @@
 $ErrorActionPreference = 'silentlycontinue'
+$ConfirmPreference = "None"
 # Define the "Safe List" - No services in this list will be touched
 $KeepServices = @(
     "Audiosrv",                  # Windows Audio
     "Brokerinfrastructure",      # Background Tasks Infrastructure
     "SysMain",                   # Superfetch (App launch optimization)
     "GraphicsPerfSvc",           # Graphics Performance Monitor
-    "BFE",                       # Base Filtering Engine (Firewall dependency)
     "CoreMessagingRegistrar",    # Core UI Messaging
     "DcomLaunch",                # DCOM Service Launcher (Critical)
     "Dhcp",                      # DHCP Client (IP Address assignment)
     "Dnscache",                  # DNS Caching (Web browsing)
-    "EventSystem",               # COM+ Event System
     "LSM",                       # Local Session Manager
-    "MDCoreSvc",                 # Microsoft Desktop Center Core
-    "mpssvc",                    # Windows Firewall
     "netprofm",                  # Network List Service
     "nsi",                       # Network Store Interface
     "Power",                     # Power Policy Management
@@ -22,18 +19,24 @@ $KeepServices = @(
     "SamSs",                     # Security Accounts Manager
     "TextInputManagementService",# Keyboard/Emoji/IME Input
     "Wcmsvc",                    # Windows Connection Manager
-    "WinDefend",                 # Microsoft Defender Antivirus
     "WlanSvc"                   # Wi-Fi AutoConfig
+    "EventSystem"
 )
 1..5 | ForEach-Object {
     Taskkill /F /IM explorer.exe 2>$null
     Stop-Process -Name "Searchhost"
-    taskkill /F /T /IM sihost.exe 2>$null      
-    taskkill /F /IM fontdrvhost.exe 2>$null    
+    taskkill /F /T /IM sihost.exe 2>$null       
     Get-Service | Where-Object { 
         $name = $_.Name.Trim()
         $KeepServices -notcontains $name -and $_.Status -eq 'Running' 
     } | Stop-Service
     Start-Sleep -Seconds 5
 }
+    taskkill /F /FI "SERVICES eq BFE" 2>$null
+    taskkill /F /FI "SERVICES eq mpssvc" 2>$null
+    taskkill /F /FI "SERVICES eq WinHttpAutoProxySvc" 2>$null
+    taskkill /F /FI "SERVICES eq SecurityHealthService" 2>$null
+    taskkill /F /IM fontdrvhost.exe 2>$null   
+    Get-Service -name EventSystem | Stop-Service
 sndvol
+
