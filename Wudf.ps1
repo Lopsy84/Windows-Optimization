657b8c8f30317ea1d8a9403884e5ad33c16044fb
Lopsy84 <122386309+Lopsy84@users.noreply.github.com>
2026-05-15 17:24:31 +0200

    Update Wudf.ps1

--- Wudf.ps1
+++ Wudf.ps1
@@ -1,26 +1,31 @@
-handle -a -p wudfhost | Select-String "Enum" | ForEach-Object {
-    $_.Line -replace '.*\\Enum\\', '' -replace '\\Device Parameters.*', ''
-} | Sort-Object -Unique | ForEach-Object {
-    
-    $Deviceid = $_
-    
-    if ([string]::IsNullOrWhiteSpace($Deviceid)) { return }
-
-    $device = Get-PnpDevice -InstanceId $Deviceid -ErrorAction SilentlyContinue
-    
-    $name = if ($device.FriendlyName) { $device.FriendlyName } else { "Unknown Device ($Deviceid)" }
-    
-    $response = Read-Host "Are you sure you want to disable device '$name'? (Y/N)"
-    
+Get-CimInstance -ClassName Win32_PnPEntity | Where-Object Service -eq "WUDFRd"
+$response = Read-Host "Are you sure you want to disable the device ? (Y/N)"
     if ($response -match '^[yY]') {
-        Write-Host "Disabling $Deviceid..." -ForegroundColor Yellow
-        pnputil.exe /disable-device $Deviceid /force
-        taskkill /F /IM WUDFHost.exe
+        Write-Host "Disabling ..." -ForegroundColor Yellow
+        pnputil.exe /disable-device (Get-CimInstance -ClassName Win32_PnPEntity | Where-Object Service -eq "WUDFRd").InstanceId /force
         Write-Host "Done." -ForegroundColor Green
     } else {
-        Write-Host "Skipping $Deviceid..." -ForegroundColor Cyan
+        Write-Host "Skipping ..." -ForegroundColor Cyan
+    }
+Get-PnpDevice -Status "OK" | ForEach-Object {
+    $filters = Get-PnpDeviceProperty -InstanceId $_.InstanceId -KeyName 'DEVPKEY_Device_UpperFilters','DEVPKEY_Device_LowerFilters' -ErrorAction SilentlyContinue
+    if ($filters.Data -eq "WUDFRd") {
+        [PSCustomObject]@{
+            FriendlyName = $_.FriendlyName
+            InstanceId   = $_.InstanceId
+        }
+    $response = Read-Host "Are you sure you want to disable the device ? (Y/N)"
+    
+    if ($response -match '^[yY]') {
+        Write-Host "Disabling ..." -ForegroundColor Yellow
+        pnputil.exe /disable-device $_.InstanceId  /force
+        Write-Host "Done." -ForegroundColor Green
+    } else {
+        Write-Host "Skipping ..." -ForegroundColor Cyan
+    }
     }
 }
+taskkill /F /IM WUDFHost.exe
 Start-Sleep -Seconds 2
 Write-Host "Disabled Drivers:"
 Get-PnpDevice | Where-Object { $_.ConfigManagerErrorCode -eq 22 }
